0fab060059012018c7aa58200ae198c5273e5574
Christopher Choi <christopherchoi@Christophers-MacBook-Pro.local>
2021-07-28 20:04:25 -0500

    finished part 3 of tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby tutorial!</p>
+      <StaticImage
+        alt="Milotic and Feebas"
+        src="../images/Milotic and Feebas finished.png"
+      />
     </Layout>
   )
 }
