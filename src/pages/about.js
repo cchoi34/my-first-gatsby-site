55f70ba97a6888dabb1840db581dfb6f3825ce17
Christopher Choi <christopherchoi@Christophers-MacBook-Pro.local>
2021-07-28 19:51:04 -0500

    added layout component for tutorial

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,16 @@
-// Step 1: Import React
+// Step 1: Import your component
 import * as React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
 // Step 2: Define your component
-const IndexPage = () => {
+const AboutPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby tutorial!</p>
+    <Layout pageTitle="About Page">
+      <p>This is the About Page. Welcome!</p>
     </Layout>
   )
 }
 
 // Step 3: Export your component
-export default IndexPage
+export default AboutPage
---
+++ src/pages/testing.js
@@ -0,0 +1,19 @@
+// Step 1: Import your component
+import * as React from 'react'
+
+// Step 2: Define your component
+const TestingPage = () => {
+  return (
+    <main>
+      <title>Testing Pate</title>
+      <h1>Testing</h1>
+      <p>Does this work?</p>
+      <div>
+        <h2>WOW this is soo cool!!!</h2>
+      </div>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default TestingPage
